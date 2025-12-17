4902f6d2c54b5cbe777e8c59c62febf13879473b
bucur <abucur@gmail.com>
2025-12-17 13:48:38 +0200

    Remove Prisma migrations - using schema-only approach

--- backend/prisma/migrations/20251216055959_add_picked_up_by_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN "pickedUpByUserId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_pickedUpByUserId_fkey" FOREIGN KEY ("pickedUpByUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
